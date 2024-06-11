3702bd7756cfd44fcaf3538b1b4fc192652d4dd1
TianaLina <66058363+TianaLina@users.noreply.github.com>
2024-06-11 19:18:49 +0300

    copy constants

---
+++ experiments/03_06_2024_markiian_ml_light_eda/constants.py
@@ -0,0 +1,7 @@
+import os
+ML_MOVIES_PATH=os.path.join('data', 'ml-1m', 'movies.dat')
+ML_RATINGS_PATH=os.path.join('data', 'ml-1m','ratings.dat')
+ML_USERS_PATH=os.path.join('data', 'ml-1m', 'users.dat')
+BC_BOOKS_PATH=os.path.join('data', 'bc', 'Books.csv')
+BC_RATINGS_PATH=os.path.join('data', 'bc', 'Ratings.csv')
+BC_USERS_PATH=os.path.join('data', 'bc', 'Users.csv')
