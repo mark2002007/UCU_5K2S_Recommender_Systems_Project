71cfe1e8100677bfdb6c29428563a6f7aeae9ee4
Markiian Mandzak <markmandzak2002@gmail.com>
2024-06-14 07:31:26 +0300

    + base class for recommenders

---
+++ src/models/base.py
@@ -0,0 +1,53 @@
+import os
+
+ROOT = os.path.join("..", "..")
+import sys
+
+sys.path.append(ROOT)
+#
+from tqdm import tqdm
+from src.metrics import ml_precision_at_k, ml_recall_at_k, ml_f1_at_k
+
+
+class BaseRecommender:
+    def __init__(self, ml_movies_df, ml_users_df):
+        self.ml_movies_df = ml_movies_df
+        self.ml_users_df = ml_users_df
+
+    def fit(self, ml_ratings_train_df):
+        raise NotImplementedError
+
+    def predict(self, user_id, n_recommendations):
+        raise NotImplementedError
+
+    def evaluate(self, ml_ratings_test_df, k):
+        users = ml_ratings_test_df["UserID"].unique()
+        history = {"precision@k": [], "recall@k": [], "f1@k": []}
+        for user_id in tqdm(users):
+            recommendations = self.predict(user_id=user_id, n_recommendations=k)
+            recommendations = recommendations.reset_index()
+            recommendations.columns = ["MovieID", "Score"]
+            precision_at_k = ml_precision_at_k(
+                k=k,
+                rec_df=recommendations,
+                ml_ratings_test_df=ml_ratings_test_df,
+                user_id=user_id,
+            )
+            recall_at_k = ml_recall_at_k(
+                k=k,
+                rec_df=recommendations,
+                ml_ratings_test_df=ml_ratings_test_df,
+                user_id=user_id,
+            )
+            f1_at_k = ml_f1_at_k(
+                k=k,
+                rec_df=recommendations,
+                ml_ratings_test_df=ml_ratings_test_df,
+                user_id=user_id,
+                precision_at_k=precision_at_k,
+                recall_at_k=recall_at_k,
+            )
+            history["precision@k"].append(precision_at_k)
+            history["recall@k"].append(recall_at_k)
+            history["f1@k"].append(f1_at_k)
+        return history
