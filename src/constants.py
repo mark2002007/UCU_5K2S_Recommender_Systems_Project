c530d4d0966d0681be3a3c41ea82c08cd32175bb
Markiian Mandzak <markmandzak2002@gmail.com>
2024-06-15 16:28:50 +0300

    ^ moved train-test split

--- src/constants.py
+++ src/constants.py
@@ -6,3 +6,5 @@ ML_USERS_PATH = os.path.join("data", "ml-1m", "users.dat")
 BC_BOOKS_PATH = os.path.join("data", "bc", "Books.csv")
 BC_RATINGS_PATH = os.path.join("data", "bc", "Ratings.csv")
 BC_USERS_PATH = os.path.join("data", "bc", "Users.csv")
+
+T_SPLIT_POINT = 970_000_000
