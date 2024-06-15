6d76e14366332dc6164bc60177cffd6e97b9001b
Markiian Mandzak <markmandzak2002@gmail.com>
2024-06-15 08:41:58 +0300

    ^ formatting

--- src/models/base.py
+++ src/models/base.py
@@ -20,34 +20,43 @@ class BaseRecommender:
     def predict(self, user_id, n_recommendations):
         raise NotImplementedError
 
-    def evaluate(self, ml_ratings_test_df, k):
+    def evaluate(self, ml_ratings_test_df, k, verbose=False):
         users = ml_ratings_test_df["UserID"].unique()
         history = {"precision@k": [], "recall@k": [], "f1@k": []}
-        for user_id in tqdm(users):
-            recommendations = self.predict(user_id=user_id, n_recommendations=k)
-            recommendations = recommendations.reset_index()
-            recommendations.columns = ["MovieID", "Score"]
-            precision_at_k = ml_precision_at_k(
-                k=k,
-                rec_df=recommendations,
-                ml_ratings_test_df=ml_ratings_test_df,
-                user_id=user_id,
-            )
-            recall_at_k = ml_recall_at_k(
-                k=k,
-                rec_df=recommendations,
-                ml_ratings_test_df=ml_ratings_test_df,
-                user_id=user_id,
-            )
-            f1_at_k = ml_f1_at_k(
-                k=k,
-                rec_df=recommendations,
-                ml_ratings_test_df=ml_ratings_test_df,
-                user_id=user_id,
-                precision_at_k=precision_at_k,
-                recall_at_k=recall_at_k,
-            )
-            history["precision@k"].append(precision_at_k)
-            history["recall@k"].append(recall_at_k)
-            history["f1@k"].append(f1_at_k)
+        with tqdm(total=len(users)) as pbar:
+            sum_f1 = 0
+            cnt_f1 = 0
+            for user_id in users:
+                recommendations = self.predict(user_id=user_id, n_recommendations=k)
+                recommendations = recommendations.reset_index()
+                recommendations.columns = ["MovieID", "Score"]
+                precision_at_k = ml_precision_at_k(
+                    k=k,
+                    rec_df=recommendations,
+                    ml_ratings_test_df=ml_ratings_test_df,
+                    user_id=user_id,
+                )
+                recall_at_k = ml_recall_at_k(
+                    k=k,
+                    rec_df=recommendations,
+                    ml_ratings_test_df=ml_ratings_test_df,
+                    user_id=user_id,
+                )
+                f1_at_k = ml_f1_at_k(
+                    k=k,
+                    rec_df=recommendations,
+                    ml_ratings_test_df=ml_ratings_test_df,
+                    user_id=user_id,
+                    precision_at_k=precision_at_k,
+                    recall_at_k=recall_at_k,
+                )
+                history["precision@k"].append(precision_at_k)
+                history["recall@k"].append(recall_at_k)
+                history["f1@k"].append(f1_at_k)
+                
+                sum_f1 += f1_at_k
+                cnt_f1 += 1
+                if verbose: 
+                    pbar.set_postfix_str(f"Avg F1@K: {sum_f1 / cnt_f1:.2f}")
+                pbar.update(1)
         return history
