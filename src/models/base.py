09cde5b9d0708058b0036e4133c0850101ca7dda
Vladyslav Radchenko <95717403+pofce@users.noreply.github.com>
2024-06-16 00:46:53 -0700

    item-item filtering added (#6)

--- src/models/base.py
+++ src/models/base.py
@@ -5,23 +5,24 @@ import sys
 
 sys.path.append(ROOT)
 #
+import pandas as pd
 from tqdm import tqdm
 from src.metrics import ml_precision_at_k, ml_recall_at_k, ml_f1_at_k
 
 
 class BaseRecommender:
-    def __init__(self, ml_movies_df, ml_users_df):
+    def __init__(self, ml_movies_df: pd.DataFrame, ml_users_df: pd.DataFrame):
         self.ml_movies_df = ml_movies_df
         self.ml_users_df = ml_users_df
 
-    def fit(self, ml_ratings_train_df):
+    def fit(self, ml_ratings_train_df: pd.DataFrame):
         raise NotImplementedError
 
-    def predict(self, user_id, n_recommendations):
+    def predict(self, user_id: int, n_recommendations: int):
         raise NotImplementedError
 
-    def evaluate(self, ml_ratings_test_df, k, verbose=False):
-        users = ml_ratings_test_df["UserID"].unique()
+    def evaluate(self, ml_ratings_test_df: pd.DataFrame, k: int, verbose=False):
+        users = ml_ratings_test_df["UserID"].unique()[0:2500]
         history = {"precision@k": [], "recall@k": [], "f1@k": []}
         with tqdm(total=len(users)) as pbar:
             sum_f1 = 0
