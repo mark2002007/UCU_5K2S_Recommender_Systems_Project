e9c2c2c8431dd79734293756c3f647ddfe14da31
Vladyslav Radchenko <vladyslav.radchenko.ki.2019@lpnu.ua>
2024-06-16 15:33:12 +0300

    time split refactored

--- src/utils.py
+++ src/utils.py
@@ -13,14 +13,23 @@ def read_bc(root="../..") -> list[DataFrame]:
 
 
 def ml_train_test_split(ml_ratings_df, min_user_test_samples, t_split_point=970_000_000):
+
     # Split data into train and test in T_SPLIT_POINT
     ml_ratings_train_df = ml_ratings_df[ml_ratings_df['Timestamp'] < t_split_point]
     ml_ratings_test_df = ml_ratings_df[ml_ratings_df['Timestamp'] >= t_split_point]
+
     # Keep only users in test that have at least MAX_K ratings
     users_with_enough_ratings = ml_ratings_test_df['UserID'].value_counts()
     users_with_enough_ratings = users_with_enough_ratings[users_with_enough_ratings >= min_user_test_samples]
     ml_ratings_test_df = ml_ratings_test_df[ml_ratings_test_df['UserID'].isin(users_with_enough_ratings.index)]
-    # Keep only users in train that are in the test
-    users_from_test = ml_ratings_test_df['UserID'].unique()
-    ml_ratings_train_df = ml_ratings_train_df[ml_ratings_train_df['UserID'].isin(users_from_test)]
+
+    # Filter the train data to include only those users who are also in the test data
+    users_from_test = set(ml_ratings_test_df['UserID'].unique())
+    users_from_train = set(ml_ratings_train_df['UserID'].unique())
+
+    users_to_keep = users_from_test & users_from_train
+
+    ml_ratings_train_df = ml_ratings_train_df[ml_ratings_train_df['UserID'].isin(users_to_keep)]
+    ml_ratings_test_df = ml_ratings_test_df[ml_ratings_test_df['UserID'].isin(users_to_keep)]
+
     return ml_ratings_train_df, ml_ratings_test_df
